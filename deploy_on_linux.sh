03edafb62626937de30616e3e4cca02c524cebe7
Hai Dinh <hai.dinh@aptiv.com>
2021-04-02 11:50:24 +0200

    Add installation of vim extension to deploy script

--- deploy_on_linux.sh
+++ deploy_on_linux.sh
@@ -1,5 +1,7 @@
 #!/usr/bin/env bash
 
+cd $(dirname "${BASH_SOURCE[0]}")
+
 function install_dependencies
 {
     if [ "$EUID" -eq 0 ]; then
@@ -7,7 +9,11 @@ function install_dependencies
         echo ""
     fi
 }
-cd $(dirname "${BASH_SOURCE[0]}")
+function install_vim_extension
+{
+    mkdir --parents ~/.vim/pack/$1/start
+    ln --symbolic --force --relative vim_extensions/$1 ~/.vim/pack/$1/start/$1
+}
 
 echo "[INFO] Deploying TMUX configs"
 ln --symbolic --force --relative tmux.conf ~/.tmux.conf
@@ -15,4 +21,5 @@ install_dependencies xclip
 
 echo "[INFO] Deploying VIM configs"
 ln --symbolic --force --relative vimrc ~/.vimrc
+install_vim_extension nerdcommenter
 install_dependencies vim vim-gtk
